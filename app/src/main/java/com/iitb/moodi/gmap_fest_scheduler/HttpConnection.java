7329d48c54334fe1ca031558c772fd0e9d5b3484
Meet Udeshi <mudeshi1209@gmail.com>
2014-10-14 19:37:58 +0530

    Add csv downloader and parser

--- app/src/main/java/com/iitb/moodi/gmap_fest_scheduler/HttpConnection.java
+++ app/src/main/java/com/iitb/moodi/gmap_fest_scheduler/HttpConnection.java
@@ -6,35 +6,67 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.util.Log;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 public class HttpConnection {
 
     public String readUrl(String mapsApiDirectionsUrl) throws IOException {
-            String data = "";
-            InputStream iStream = null;
-            HttpURLConnection urlConnection = null;
-            try {
-                URL url = new URL(mapsApiDirectionsUrl);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.connect();
-                iStream = urlConnection.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(
-                        iStream));
-                StringBuffer sb = new StringBuffer();
-                String line = "";
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
-                }
-                data = sb.toString();
-                br.close();
-            } catch (Exception e) {
-                Log.d("Exception while reading url", e.toString());
-            } finally {
-                iStream.close();
-                urlConnection.disconnect();
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(mapsApiDirectionsUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+            iStream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    iStream));
+            StringBuffer sb = new StringBuffer();
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
             }
-            return data;
+            data = sb.toString();
+            br.close();
+        } catch (Exception e) {
+            Log.d("Exception while reading url", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
         }
+        return data;
+    }
+
+    public List<String[]> getCSV(String csvUrl) throws IOException {
+
+        List<String[]> data=new ArrayList<String[]>();
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(csvUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+            iStream = urlConnection.getInputStream();
+            CSVReader csv = new CSVReader(new InputStreamReader(
+                    iStream));
+
+            String[] next = {};
+
+            while ((next = csv.readNext()) != null) {
+                data.add(next);
+            }
+        } catch (Exception e) {
+            Log.d("Exception while reading url", e.getMessage());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+
+    return data;
+    }
 }
--- app/src/main/java/com/iitb/moodi/gmap_fest_scheduler/MainActivity.java
+++ app/src/main/java/com/iitb/moodi/gmap_fest_scheduler/MainActivity.java
@@ -3,9 +3,17 @@ package com.iitb.moodi.gmap_fest_scheduler;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -13,33 +21,87 @@ public class MainActivity extends Activity {
 
     // 19.130739, 72.917208 lec hall
     RadioGroup list;
+    List<String[]> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list= (RadioGroup) findViewById(R.id.event_list);
+
+        new ReadTask().doInBackground("https://drive.google.com/uc?export=download&id=0B_6rvZNWXShpV0t0ZWxlNkQ5UE0");
     }
 
     public void onClick(View v){
         int radioButtonID = list.getCheckedRadioButtonId();
-        String latlang="";
-
-        switch(radioButtonID){
-            case R.id.convo:
-                latlang="19.132069,72.914452";
-                break;
-            case R.id.lec_hall:
-                latlang="19.130739,72.917208";
-                break;
-            case R.id.vmcc:
-                latlang="19.132479,72.917279";
-                break;
-        }
+        String latlang=getLatLang(data.get(radioButtonID)[4]);
 
         Intent intent = new Intent();
-        intent.putExtra("url","https://maps.googleapis.com/maps/api/directions/json?destination="+latlang);
+        intent.putExtra("url","https://maps.googleapis.com/maps/api/directions/json?origin=19.137559,72.914470destination="+latlang);
         intent.setClass(this, PathDisplayActivity.class);
         startActivity(intent);
+    }
+
+    private String getLatLang(String place){
+        if(place.equals("LCH"))
+            return "19.130739,72.917208";
+        else if(place.equals("SAC"))
+            return "19.135369,72.913769";
+        else if(place.equals("Footer Field"))
+            return "19.134354,72.912133";
+        else if(place.equals("VMCC"))
+            return "19.132506,72.917260";
+        else if(place.equals("FCK"))
+            return "19.130484,72.915719";
+        else if(place.equals("H8 Road"))
+            return "19.133731,72.911319";
+        else if(place.equals("KV Grounds"))
+            return "19.129144,72.918190";
+        else
+            return "19,72";
+    }
+
+    private class ReadTask extends AsyncTask<String, Integer, List<String[]>> {
+        @Override
+        protected List<String[]> doInBackground(String... url) {
+            List<String[]> data = new ArrayList<String[]>();
+            try {
+                HttpConnection http = new HttpConnection();
+                data = http.getCSV(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            if(progress[0]%10!=0) return;
+
+            Toast t = new Toast(getApplicationContext());
+            t.setText("Downloaded "+ progress[0]+"%");
+            t.setDuration(Toast.LENGTH_SHORT);
+        }
+
+        @Override
+        protected void onPostExecute(List<String[]> result) {
+            super.onPostExecute(result);
+            data=result;
+            populateList();
+        }
+    }
+
+    private void populateList(){
+        list= (RadioGroup) findViewById(R.id.event_list);
+
+        for(int i=0; i<data.size(); i++){
+            String[] event = data.get(i);
+            RadioButton rb = new RadioButton(this);
+
+            rb.setText(event[0]+" @ "+event[4]);
+            rb.setId(i);
+            if(i==0) rb.setSelected(true);
+
+            list.addView(rb);
+        }
     }
 }
